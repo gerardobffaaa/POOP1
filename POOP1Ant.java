bed7fb98086f7d8ee1a53845db5909c59b88e421
gerardobffaaa <gerardobffaaa@hotmail.com>
2025-03-20 20:09:07 -0600

    Add files via upload

---
+++ POOP1Ant.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poop1.ant;
+
+/**
+ *
+ * @author Orozco Mora Gerardo
+ */
+public class POOP1Ant {
+    /**
+     * Programa sencillo para iniciar con java
+     */
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Hola mundo
+        System.out.println("Hola Mundo");
+        System.out.println("Desde POO");
+        // Multiplicacion
+        int numero1=3;
+        int numero2=4;
+        int resultado= numero1*numero2;
+         System.out.println("La Multiplicacion es "+resultado);
+        // Condicional if
+        int numero=12;
+        if(numero>10)
+            System.out.println("El numero "+numero+ " es mayor que 10");
+         
+    }
+    
+}
